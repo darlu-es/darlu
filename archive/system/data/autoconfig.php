23ca7bda38ac3850b743092fac39286f2227f588
d.Arlu <hola@darlu.es>
2020-07-12 19:22:01 +0200

    Seguridad

--- archive/system/data/autoconfig.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$config['db'] = array (
-  'type' => NULL,
-  'server' => 'PMYSQL110.dns-servicio.com',
-  'database' => '6815602_archive',
-  'username' => 'user_archive',
-  'password' => 'Ujfj7!32',
-);
--- url/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-    header('Location: https://darlu.es/url/admin/');
-    $fh = fopen('/tmp/track.txt', 'a');
-    fwrite($fh, $_SERVER['REMOTE_ADDR'] . ' ' . date('c') . "\n");
-    fclose($fh);
-?>
